109047af0040bcf0e42b24d0934a1a355f3e52df
Tatiana Isaicheva <mujer-amante@yandex.ru>
2020-09-27 15:45:06 +0300

    feat: сборка проекта webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: { main: './src/pages/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                // регулярное выражение, которое ищет все файлы с такими расширениями
+                test: /\.(png|svg|jpg|gif|ttf|woff|woff2)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                // применять это правило только к CSS-файлам
+                test: /\.css$/,
+                // при обработке этих файлов нужно использовать
+                // MiniCssExtractPlugin.loader и css-loader
+                loader: [MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: { importLoaders: 1 }
+                },
+                    'postcss-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
